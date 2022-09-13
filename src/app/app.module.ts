fef430dbfa6865d9f5cd27f883b1eb7bcfdf05af
Maya <37115588+Maya084@users.noreply.github.com>
2022-09-13 08:36:07 +0200

    ♻️  Refactoring: refactor form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
 import { ListComponent } from './components/list/list.component';
 import { RatingSelectComponent } from './components/rating-select/rating-select.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,7 +15,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,49 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { FeedbackService } from '../../services/feedback.service';
 import { v4 as uuidv4 } from 'uuid';
-import { animate, style, transition, trigger } from '@angular/animations';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css'],
-  animations: [
-    trigger('enableDisable', [
-      transition('enabled <=> disabled', animate('1s cubic-bezier(0.8,0.3,0,1)')),
-    ])
-  ]
 })
 export class FormComponent implements OnInit {
   btnDisabled = true;
   minLength = 10;
-  commentValue = '';
   rating = 10;
+
+  formFeedback = new FormControl('', [Validators.minLength(this.minLength)]);
+
   constructor(
-    private service: FeedbackService
+    private service: FeedbackService,
   ) { }
 
   ngOnInit() {
   }
 
   handleSubmit() {
-    if (this.commentValue.trim().length < this.minLength) {
+    if (this.formFeedback.invalid) {
       return;
     }
 
     const newFeedback = {
       id: uuidv4(),
-      comment: this.commentValue,
+      comment: this.formFeedback.value,
       rating: this.rating
     };
 
     this.service.add(newFeedback);
-  }
-
-  onChangeInput(value: any) {
-    console.log('onChangeInput');
-
-    this.commentValue = value;
-    this.btnDisabled = this.commentValue.trim().length < this.minLength;
   }
 
   getRating(ratingValue: any): void {
